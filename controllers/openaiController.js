4ab991f1a520661ae1c178ec1d574155a249f510
MajdQumseya <qumseya.majd@gmail.com>
2023-01-25 13:22:32 +0200

    new API Key

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,13 +1,13 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
 
 const generateImage = async (req, res) => {
-    const {prompt, size} = req.body
+    const { prompt, size } = req.body
     console.log(prompt, size)
     const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
     try {
