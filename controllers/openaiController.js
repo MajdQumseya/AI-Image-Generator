53930d9f1d062edd0d9979cd5ade459ef590f4bb
MajdQumseya <qumseya.majd@gmail.com>
2023-01-25 13:31:42 +0200

    changed api key

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-    apiKey: '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',
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
